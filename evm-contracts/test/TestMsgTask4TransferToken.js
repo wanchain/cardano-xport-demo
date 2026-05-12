d5c4ba5c3a52663cbdbdaaf6cd35d0f14d202c42
Orel <38098634+Orel-bytes@users.noreply.github.com>
2026-05-12 10:13:01 +0800

    to

--- evm-contracts/test/TestMsgTask4TransferToken.js
+++ evm-contracts/test/TestMsgTask4TransferToken.js
@@ -11,7 +11,7 @@ const TokenHomeSc = require("../deployed/scAbi/ERC20TokenHome4CardanoV2.json");
 const GXTOKEN_SCADDRESS = Deployed_WanChain.XToken; 
 const LOCAL_TOKENHOME_SCADDRESS = Deployed_WanChain.TokenHome; 
 const WAITTING_SECONDS = 30*1000;
-// 等待 N 秒
+
 function sleep(time) {
 	return new Promise(function (resolve, reject) {
 		setTimeout(function () {
@@ -53,20 +53,17 @@ describe("\n\n****Test ERC20TokenHome4Cardano", function () {
       console.log("balance of owner :", ownerBalanceBefore);
       console.log("balance of tokenHome:", addr1BalanceBefore);
 
-      // addr1 批准 owner 可以从其账户转走 token
       const approveAmount = 100000000000;
       await gxTokenScInst.approve(testAccount.address, approveAmount);
       console.log("\nto approve token for owner..");
       await sleep(WAITTING_SECONDS);
 
-      // owner 使用 transferFrom 从 addr1 转给 addr2
       const transferAmount = 1000000000;
       // await gxTokenScInst.transferFrom(owner.address, LOCAL_TOKENHOME_SCADDRESS, transferAmount);
       await gxTokenScInst.transferFrom(owner.address, testAccount.address, transferAmount);
       console.log("transfer token to tokenHome sc..");
       await sleep(WAITTING_SECONDS);
 
-      // 转账后余额
       const ownerBalanceAfter = await gxTokenScInst.balanceOf(owner.address);
       const addr1BalanceAfter = await gxTokenScInst.balanceOf(testAccount.address);
       console.log("\nbalance of owner after transfer:",ownerBalanceAfter);
